cfdd40234ea143263742dfecfb8b4d1f41306e11
JiyaRaj <86504048+JiyaRaj@users.noreply.github.com>
2021-12-26 10:49:03 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,49 +1,30 @@
-var names_of_people = [];
-    
-function submit()
-{
-    var GuestName = document.getElementById("name1").value;
-	names_of_people.push(GuestName);
-	document.getElementById("display_name").innerHTML=names_of_people.toString();
-	console.log(names_of_people);
-    var lenght_of_name = names_of_people.length;
-    console.log(lenght_of_name);
-	console.log(GuestName); 
-	
-   }
+var student_arry =[
+];
+var display_array=[];
 
-   
-
-function show()
+function submit(){
+    for(var i=1; i<=4; i++)
 {
-	var i= names_of_people.join("<br>");
-	console.log(names_of_people);
-	document.getElementById("p1").innerHTML=i.toString();
-	document.getElementById("sort_button").style.display="block";
-	
+    var name= document.getElementById("name_of_the_student_"+i).value;
+    student_arry.push(name);
 }
 
-
-function sorting()
-	{
-		names_of_people.sort()
-		var i= names_of_people.join("<br>");
-		console.log(names_of_people);		
-		document.getElementById("sorted").innerHTML=i.toString();
-		}
-
-
-function searching()
+for(var i=0; i<student_arry.length; i++)
 {
-	var s= document.getElementById("s1").value;
-	var found=0;
-	var j;
-	for(j=0; j<names_of_people.length; j++)
-		{
-			if(s==names_of_people[j]){
-				found=found+1;
-			}	
-		}
-	document.getElementById("p2").innerHTML="name found "+found+" time/s";
-	console.log("found name "+found+" time/s");
+    display_array.push("<h4> name - " + student_arry[i] + "</h4>")
+}
+document.getElementById("display_name_with_commas").innerHTML= display_array;
+var remove_commas= display_array.join(" ");
+document.getElementById("display_name_without_commas").innerHTML= remove_commas;
+document.getElementById("submit_button").style.display="none";
+document.getElementById("sort_button").style.display="inline-block";
+}
+
+function sorting(){
+student_arry.sort();
+var display_arry_sort=[];
+for(var i=0; i<student_arry.length; i++){
+    display_arry_sort.push("<h4> name - " + student_arry[i] + "</h4>");
+}
+document.getElementById("display_name_with_commas").innerHTML= display_arry_sort;
 }
